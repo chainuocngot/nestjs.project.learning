d560b3d1b1b8063dc80338f6284bf850f4195fcd
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-04-13 14:46:02 +0700

    Tạo script xóa hoặc tạo permission dựa trên các endpoint hiện có

--- initialScript/create-permissions.ts
+++ initialScript/create-permissions.ts
@@ -11,11 +11,21 @@ async function bootstrap() {
   const server = app.getHttpAdapter().getInstance();
   const router = server.router;
 
-  const availableRoutes = router.stack
+  const permissionsInDb = await prisma.permission.findMany({
+    where: {
+      deletedAt: null,
+    },
+  });
+
+  const availableRoutes: {
+    path: string;
+    method: (typeof HTTPMethod)[keyof typeof HTTPMethod];
+    name: string;
+  }[] = router.stack
     .map((layer) => {
       if (layer.route) {
         const path = layer.route?.path;
-        const method = String(layer.route?.stack[0].method).toUpperCase() as keyof typeof HTTPMethod;
+        const method = String(layer.route?.stack[0].method).toUpperCase();
 
         return {
           path,
@@ -26,12 +36,49 @@ async function bootstrap() {
     })
     .filter((item) => item !== undefined);
 
-  const result = await prisma.permission.createMany({
-    data: availableRoutes,
-    skipDuplicates: true,
+  const permissionsInDbMap: Record<string, (typeof permissionsInDb)[0]> = permissionsInDb.reduce((acc, item) => {
+    acc[`${item.method}-${item.path}`] = item;
+
+    return acc;
+  }, {});
+
+  const availableRoutesMap: Record<string, (typeof availableRoutes)[0]> = availableRoutes.reduce((acc, item) => {
+    acc[`${item.method}-${item.path}`] = item;
+
+    return acc;
+  }, {});
+
+  const permissionsToDelete = permissionsInDb.filter((item) => {
+    return !availableRoutesMap[`${item.method}-${item.path}`];
   });
 
-  console.log(result);
+  if (permissionsToDelete.length > 0) {
+    const deleteResult = await prisma.permission.deleteMany({
+      where: {
+        id: {
+          in: permissionsToDelete.map((item) => item.id),
+        },
+      },
+    });
+    console.log('Deleted permissions:', deleteResult.count);
+  } else {
+    console.log('No permissions to delete');
+  }
+
+  const routesToAdd = availableRoutes.filter((item) => {
+    return !permissionsInDbMap[`${item.method}-${item.path}`];
+  });
+
+  if (routesToAdd.length > 0) {
+    const permissionsToAdd = await prisma.permission.createMany({
+      data: routesToAdd,
+      skipDuplicates: true,
+    });
+    console.log('Added permissions:', permissionsToAdd.count);
+  } else {
+    console.log('No permissions to add');
+  }
+
   process.exit(0);
 }
 
