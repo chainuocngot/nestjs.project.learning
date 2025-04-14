7d729ce5820103ff740e6d4f6ba033a7313d1ff7
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-04-14 22:25:52 +0700

    Refactor RolesService trong auth

--- src/shared/repositories/shared-role.repo.ts
+++ src/shared/repositories/shared-role.repo.ts
@@ -6,16 +6,13 @@ import { PrismaService } from 'src/shared/services/prisma.service';
 @Injectable()
 export class SharedRoleRepository {
   private clientRoleId: number | null = null;
+  private adminRoleId: number | null = null;
 
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getClientRoleId() {
-    if (this.clientRoleId) {
-      return this.clientRoleId;
-    }
-
+  private async getRole(roleName: string) {
     const role: RoleType = await this.prismaService
-      .$queryRaw`SELECT * FROM "Role" WHERE name = ${RoleName.Client} AND "deletedAt" IS NULL LIMIT 1;`.then(
+      .$queryRaw`SELECT * FROM "Role" WHERE name = ${roleName} AND "deletedAt" IS NULL LIMIT 1;`.then(
       (res: RoleType[]) => {
         if (res.length === 0) {
           throw new Error('Client role not found');
@@ -25,7 +22,28 @@ export class SharedRoleRepository {
       },
     );
 
+    return role;
+  }
+
+  async getClientRoleId() {
+    if (this.clientRoleId) {
+      return this.clientRoleId;
+    }
+
+    const role = await this.getRole(RoleName.Client);
+
     this.clientRoleId = role.id;
+    return role.id;
+  }
+
+  async getAdminRoleId() {
+    if (this.adminRoleId) {
+      return this.adminRoleId;
+    }
+
+    const role = await this.getRole(RoleName.Admin);
+
+    this.adminRoleId = role.id;
     return role.id;
   }
 }
